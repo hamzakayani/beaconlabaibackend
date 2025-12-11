b75b1277f3d7ff0a38b8c09c25d1f120877f4214
HabzaYasin-29 <hubza.yasin@funavry.com>
2025-12-11 12:55:38 +0500

    Add database configuration and migration setup with Alembic

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+class Settings(BaseSettings):
+    DATABASE_URL: str #= "sqlite:///./sql_app.db"
+    SECRET_KEY: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
